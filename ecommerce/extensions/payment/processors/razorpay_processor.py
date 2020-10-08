ca417ed6bd1c4b860d30ddfa988bdf41108eab4c
Raifox <shashi.kiran@raifox.com>
2020-10-08 14:45:57 +0530

    Added bootstrap support to razorpay.html page

--- ecommerce/extensions/payment/processors/razorpay_processor.py
+++ ecommerce/extensions/payment/processors/razorpay_processor.py
@@ -45,9 +45,6 @@ class RazorpayProcessor(BasePaymentProcessor):
     def error_url(self):
         return get_ecommerce_url(self.configuration['error_path'])
 
-    def _get_basket_amount(self, basket):
-        return int(basket.total_incl_tax * 100)
-
     def get_transaction_parameters(self, basket, request=None, use_client_side_checkout=False, **kwargs):
         """
         Generate a dictionary of signed parameters Razorpay requires to complete a transaction.
@@ -84,7 +81,7 @@ class RazorpayProcessor(BasePaymentProcessor):
              dict: Dictionary containing the payment parameters that should be sent to CyberSource.
         """
         parameters = {
-            'amount': self._get_basket_amount(basket),
+            'amount': int(basket.total_incl_tax),
             'currency': 'INR',
             'receipt': uuid.uuid4().hex,
             'payment_capture': 1
@@ -135,8 +132,6 @@ class RazorpayProcessor(BasePaymentProcessor):
         parameters['razorpay_api_key'] = self.api_key
         parameters['payment_page_url']  = "/payment/razorpay/form/" + str(basket.id) + "/"
 
-        print('PARAMETERS')
-        print(parameters)
         return parameters
 
     def handle_processor_response(self, response, basket=None):
